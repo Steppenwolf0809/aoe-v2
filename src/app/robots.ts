9819dd6c24e7cfafb97685c00f19621be5079abc
Steppenwolf0809 <joseluiszapataec@gmail.com>
2026-09-24 17:08:00 -0500

    feat(robots): permitir /api/cotizar

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: ['/', '/api/cotizar'],
       disallow: ['/api/', '/dashboard/', '/perfil/', '/contratos/', '/documentos/', '/suscripcion/'],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
